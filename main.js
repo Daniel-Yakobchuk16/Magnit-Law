984571f02cdcfe9ab6b7bf41a2506e0f67060486
Daniel <73886324+Daniel-Yakobchuk16@users.noreply.github.com>
2023-07-17 18:10:50 +0300

    Mag

--- main.js
+++ main.js
@@ -130,44 +130,45 @@ $("#offer_cards_buisnes_more, #offer_cards_private_more").on("click", function(e
 // slider
 
 const slider = document.querySelector(".items");
-		const slides = document.querySelectorAll(".item");
-		const button = document.querySelectorAll(".button");
+const slides = document.querySelectorAll(".item");
+const button = document.querySelectorAll(".button");
 
-		let current = 0;
-		let prev = 4;
-		let next = 1;
+let current = 0;
+let prev = 6;
+let next = 1;
 
-		for (let i = 0; i < button.length; i++) {
-			button[i].addEventListener("click", () => i == 0 ? gotoPrev() : gotoNext());
-		}
-
-		const gotoPrev = () => current > 0 ? gotoNum(current - 1) : gotoNum(slides.length - 1);
-
-		const gotoNext = () => current < 4 ? gotoNum(current + 1) : gotoNum(0);
-
-const gotoNum = number => {
-			current = number;
-			prev = current - 1;
-			next = current + 1;
-
-			for (let i = 0; i < slides.length; i++) {
-				slides[i].classList.remove("active");
-				slides[i].classList.remove("prev");
-				slides[i].classList.remove("next");
-			}
-
-			if (next == 5) {
-				next = 0;
-			}
-
-			if (prev == -1) {
-				prev = 4;
-			}
-
-			slides[current].classList.add("active");
-			slides[prev].classList.add("prev");
-			slides[next].classList.add("next");
+for (let i = 0; i < button.length; i++) {
+  button[i].addEventListener("click", () => (i == 0 ? gotoPrev() : gotoNext()));
 }
+
+const gotoPrev = () => (current > 0 ? gotoNum(current - 1) : gotoNum(slides.length - 1));
+
+const gotoNext = () => (current < 6 ? gotoNum(current + 1) : gotoNum(0));
+
+const gotoNum = (number) => {
+  current = number;
+  prev = current - 1;
+  next = current + 1;
+
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].classList.remove("active");
+    slides[i].classList.remove("prev");
+    slides[i].classList.remove("next");
+  }
+
+  if (next == 7) {
+    next = 0;
+  }
+
+  if (prev == -1) {
+    prev = 6;
+  }
+
+  slides[current].classList.add("active");
+  slides[prev].classList.add("prev");
+  slides[next].classList.add("next");
+};
+
 
 $(document).ready(function() {
   // Отображение стрелки при прокрутке вниз
@@ -184,3 +185,8 @@ $(document).ready(function() {
     $('html, body').animate({ scrollTop: 0 }, '300');
   });
 });
+
+
+
+
+
